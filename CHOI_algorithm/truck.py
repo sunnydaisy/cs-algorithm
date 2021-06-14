3f9c2eda2e50bfe40586fe41f140db372e599e5a
jhon3242 <78288539+jhon3242@users.noreply.github.com>
2021-06-14 21:24:49 +0900

    truck 문제 시도2(해결)
    리스트를 여러개 만들고 check_time이라는 새로운 리스트를 만들어서 시도해보았다.
    이 리스트에는 완료되는 시간의 데이터를 넣어서 만약 더이상 다리 위에 추가할 수 없을 때 현재 시간을 check_time리스트의 0번 index의 값을 현재 시간으로 바꿔줌으로서 다리에서 트럭 하나가 빠지고 기다리고 있는 트럭이 추가되도록 코드를 작성하였다.

--- CHOI_algorithm/truck.py
+++ CHOI_algorithm/truck.py
@@ -1,33 +1,35 @@
 def solution(bridge_length, weight, truck_weights):
-    next = truck_weights.pop(0)
-    passing_list = []
-    passing_list_weight = 0
-    time = 0
-    passed_list = []
+    time = 1
+    passed_truck=[]
+    passing_truck=[]
+    truck_num = len(truck_weights)
+    waiting_truck = truck_weights
+    passing_weight = 0
+    check_time = []
 
-    # 루프
-    # 트럭 빼기
-    while True:
-        for truck in passing_list:
-            if truck[1] == bridge_length:
-                passed_list.append(truck[0])
-                passing_list.pop(truck)
-            else :
-                # 모두 1시간 추가
-                truck[1] += 1
 
-        # 트럭 추가
-        if weight >= passing_list_weight + next:
-            passing_list.append((next, 0))
+    #### 루프
+    while len(passed_truck) != truck_num:
+        # check_time 가 현재 시간일때
+        if len(check_time) !=0 and time == check_time[0]:
+            # 시간 체크 리스트에서 지우기
+            check_time.remove(check_time[0])
+            # 건넌 트럭 리스트에 추가
+            passed_truck.append(passing_truck.pop(0))
+            # 건너는중 무게에서 빼기
+            passing_weight -= passed_truck[-1]
 
-        if len(passed_list) == len(truck_weights):
-            break
-        time += 1
+
+
+        # 건널 수 있으면
+        if len(waiting_truck) !=0 and weight >= passing_weight + waiting_truck[0] and bridge_length >= len(passing_truck)+1:
+            passing_weight += waiting_truck[0]
+            passing_truck.append(waiting_truck.pop(0))
+            check_time.append(time+bridge_length)
+            time += 1
+        elif len(waiting_truck)==0:
+            return check_time[-1]
+        else:
+            time = check_time[0]
+
     return time
-
-
-bridge_length = 2
-weight = 10
-truck_weights = [7,4,5,6]
-
-print(solution(bridge_length, weight, truck_weights))
