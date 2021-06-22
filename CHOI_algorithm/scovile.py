c163b045c0a3259dc1715fc3318ce59f8a6c41c1
jhon3242 <78288539+jhon3242@users.noreply.github.com>
2021-06-22 21:47:42 +0900

    scovile 문제 시도-2
    https://programmers.co.kr/learn/courses/30/lessons/42626?language=python3
    pop을 통해 시도해 보았고 정확성에서는 통과했지만 효율성에서는 통과를 못함

--- CHOI_algorithm/scovile.py
+++ CHOI_algorithm/scovile.py
@@ -1,13 +1,14 @@
 def solution(scoville, K):
     count = 0
     while len(scoville) > 1:
-        scoville.sort(reverse=True)
-        if scoville[-1] >=K:
+        if min(scoville) >= K:
             return count
-        new_food = scoville[-1] + scoville[-2] * 2
-        scoville = scoville[:-2]
-        scoville.append(new_food)
+        f_min_scoville = scoville.pop(scoville.index(min(scoville)))
+        s_min_scoville = scoville.pop(scoville.index(min(scoville)))*2
+        scoville.append(f_min_scoville+s_min_scoville)
+        print(scoville)
         count += 1
+
     if scoville[0] >= K:
         return count
 
