52fe2eb4d41e237e7bdc18d0827042e5d83b3aaf
jhon3242 <78288539+jhon3242@users.noreply.github.com>
2021-07-22 14:19:36 +0900

    나머지 문제 (백준 3052번) 해결
    함수를 새로 정의해서 이미 배열 안에 있던 원소면 0을 없는 원소면 1을 리턴하게 하여 만약 없던 원소면 배열에 추가하고 answer 에 +1 을 하는 형태로 바꾸어 추가된 횟수를 구하고 답으로 출력하는 방식으로 하였더니 문제가 풀렸다. (만약 나머지가 0인 경우 그냥 배열에 0을 추가하면 null 값과 구분이 안돼 -1을 추가하도록 하였다.)

--- CHOI_algorithm/array_repetition.c
+++ CHOI_algorithm/array_repetition.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
+int check(int *arr, int index, int num);
 int main(){
-    int count=0,num,index=0,input;
-    int arr[10];
-    while (count<10){
-        int yes=1;
-        scanf("%d",&input);
-        num = input % 42;
-        if (num==0) num = -1;
-        /* 배열에 있는 값인지 확인 */
-        int i=0;
-        while (i<10){
-            if(arr[i] == num) {
-                /* 이미 있는 값일 때*/
-                yes = 0;
-                break;
-            }
-            i++;
+    int num;
+    int arr[10]={};
+    int answer = 0;
+
+
+    /* 배열의 원소 넣기
+     배열은 배열 원소에 있지 않은 값이면 넣고 만약 이미 있는 원소면 넣지 않는다.
+     */
+    for (int i=0;i<10;i++){
+        scanf("%d",&num);
+        num = num % 42;
+        if (num == 0) num = -1;
+        if (check(arr, i, num)){
+            arr[i] = num;
+            answer ++;
         }
-        if (yes) {
-            arr[index] = num;
-            index ++;
-        }
-        count ++;
     }
-    printf("%d\n",index);
+
+    printf("%d",answer);
+
+
     return 0;
+}
+
+/* 이미 있던 원소인지 확인하는 함수
+    처음 보는 원소이면 1을 리턴
+    이미 배열안에 있는 원소면 0을 리턴
+*/
+
+int check(int *arr, int index, int num){
+    int i=0;
+    while (i<index){
+        if (arr[i] == num){
+            return 0;
+        }
+        i++;
+    }
+    return 1;
 }
