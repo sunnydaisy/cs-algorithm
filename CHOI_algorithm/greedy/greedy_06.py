70448f33661ac40c1dbf03d3a91e5b43f853713e
jhon3242 <rldotnr2@gmail.com>
2022-03-08 16:48:42 +0900

    그리디 6번 (중단) 계속 시도해보다가 결국 그리디 6번의 해설을 보았는데 아직 배우지 않은 힙큐를 사용하여 일단은 2단계 기본 문제부터 풀어가면서 새로운 모듈을 배워야 한다고 판단했다. 일단 6번 풀이를 중단하고 2단계를 진행하기로 했다.

--- CHOI_algorithm/greedy/greedy_06.py
+++ CHOI_algorithm/greedy/greedy_06.py
@@ -64,17 +64,72 @@
 # 	return -1
 
 
+# 시도 4 (테스트케이스 : 실패 / 효율성 : 실패)
+# 코드를 바꿔보려고 하다가 둘다 실패했다.
 
-def solution(food_times, k):
-	loop = 0
-	if k > len(food_times):
-		ct = sum(i > loop for i in food_times)
+# def solution(food_times, k):
+# 	loop = 0
+# 	food_ct = len(food_times)
+# 	ct = 0
+# 	if k > len(food_times):
+# 		loop = min(food_times)
+# 		if sum(i > loop for i in food_times) == 0:
+# 			loop -= 1
+# 		ct = food_ct * loop
+# 	for i in range(food_ct):
+# 		if food_times[i] > loop:
+# 			ct += 1
+# 			if k + 1 == ct:
+# 				return i + 1
+# 	loop += 1
+# 	for i in range(food_ct):
+# 		if food_times[i] > loop:
+# 			ct += 1
+# 			if k + 1 == ct:
+# 				return i + 1
+# 	return -1
+
+# 시도 5 (테스트케이스 : 실패 / 효율성 : 실패)
+# 조건에 만족하는 요소의 개수만큼 더해서 k 보다 커지는 경우 종료하는 형식으로 하려고 했는데 역시 실패했다.
+
+# def solution(food_times, k):
+# 	loop = 0
+# 	food_ct = len(food_times)
+# 	# if k + 1 > food_ct:
+# 	# 	loop = min(food_times)
+# 	while True:
+# 		tmp_ct = sum(i > loop + 1 for i in food_times)
+# 		if tmp_ct == 0 :
+# 			return -1
+# 		if food_ct + tmp_ct > k + 1:
+# 			loop += 1
+# 			break
+# 		food_ct += tmp_ct
+# 		loop += 1
+# 	for i in range(len(food_times)):
+# 		if food_times[i] > loop:
+# 			food_ct += 1
+# 			if k + 1 == food_ct:
+# 				return i + 1
+# 	return -1
 
 
+# import heapq
 
+# def solution(food_times, k):
+# 	if sum(food_times) <= k:
+# 		return -1
+	
+# 	q = []
+# 	for i in range(len(food_times)):
+# 		heapq.heappush(q, (food_times[i], i + 1))
+	
+# 	length = len(food_times)
+# 	past_time = 0
 
+	
 
-food_times = [3, 1, 2]
-food_times > 2
-k = 5
-# print(solution(food_times, k))
+food_times = [4,2,3,6,7,1,5,8]
+
+k = 16
+print(solution(food_times, k))
