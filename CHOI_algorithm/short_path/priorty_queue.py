1d93a8bd17c4b4d8d199271e853beaa2a03c3ec0
jhon3242 <rldotnr2@gmail.com>
2022-03-20 23:36:24 +0900

    최단거리 다익스트라 알고리즘 구현

---
+++ CHOI_algorithm/short_path/priorty_queue.py
@@ -0,0 +1,41 @@
+from dis import dis
+import heapq
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m = map(int, input().split())
+
+start = int(input())
+# 각 노드에 연결되어 있는 노드에 대하 정보를 담는 리스트
+graph = [[] for i in range(n + 1)]
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+	a, b, c = map(int, input().split())
+	# a 노드에서 b 로 가는 비용이 c 라는 의미
+	graph[a].append((b, c))
+
+def dijkstra(start):
+	q = []
+	heapq.heappush(q, (0, start))
+	distance[start] = 0
+	while q:
+		dist, now = heapq.heappop(q)
+		if distance[now] < dist:
+			continue
+		for i in graph[now]:
+			cost = dist + i[1]
+			if cost < distance[i[0]]:
+				distance[i[0]] = cost
+				heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+	if distance[i] == INF:
+		print("INFINITY")
+	else:
+		print(distance[i])
