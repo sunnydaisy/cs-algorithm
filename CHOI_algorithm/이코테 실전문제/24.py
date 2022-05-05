ffdbf7ecc77930aa42d01c407594bf8018ed297e
jhon3242 <rldotnr2@gmail.com>
2022-05-05 23:00:07 +0900

    test

--- CHOI_algorithm/이코테 실전문제/24.py
+++ CHOI_algorithm/이코테 실전문제/24.py
@@ -5,6 +5,14 @@
 def solution():
 	n = int(input())
 	house = map(int, input().split())
-	
+	num_list = [0] * 100001
+	for i in house:
+		num_list[i] += 1
+	result = int(1e9)
+	for i in range(1, 100001):
+		if num_list[i] > 0:
+			result = 0
+			for j in range(1, 100001):
+				
 
 solution()
