bbbcf4d2f24c9da489bc46aebe09243adc291105
jhon3242 <rldotnr2@gmail.com>
2023-04-15 15:00:03 +0900

    feat(programers) : 그리디 문제풀이 추가

---
+++ CHOI_algorithm/programers/[Lv1]체육복.js
@@ -0,0 +1,27 @@
+/**
+ * 1 차 시도 실패 92/100
+ */
+
+function solution(n, lost, reserve) {
+    var answer = n;
+    let have = new Array(n + 1).fill(1); // 0도 포함하고 나중에 0인 사람만 n 에서 뺸다
+    lost.forEach(num => {
+        let extraIdx = reserve.indexOf(num);
+        if (extraIdx === -1) have[num] = 0;
+        else {
+            reserve[extraIdx] = 0;
+        }
+    });
+
+    reserve.forEach(num => {
+        if (num === 0) {
+            return ;
+        }
+        if (!have[num - 1]) {
+            have[num - 1] = 1;
+        } else if (!have[num + 1]) {
+            have[num + 1] = 1;
+        }
+    })
+    return n - have.filter(v => v===0).length;
+}
---
+++ CHOI_algorithm/programers/[Lv2]조이스틱.js
@@ -0,0 +1,69 @@
+/**
+ * 1차 시도 실패 74.1 / 100.0
+ */
+function leftTargetLength(name, idx) {
+    let result = 0;
+    let moveIdx = idx;
+    let flag = true;
+    while (moveIdx !== idx || flag) {
+        if (name[moveIdx] !== "A") return [result, moveIdx];
+        result++;
+        moveIdx--;
+        if (moveIdx === -1) {
+            moveIdx = name.length - 1;
+        }
+        flag = false;
+    };
+    return [-1, -1];
+};
+
+function rightTargetLength(name, idx) {
+    let result = 0;
+    let moveIdx = idx;
+    let flag = true;
+    while (moveIdx !== idx || flag) {
+        if (name[moveIdx] !== "A") return [result, moveIdx];
+        result++;
+        moveIdx++;
+        if (moveIdx === name.length) {
+            moveIdx = 0;
+        }
+        flag = false;
+    };
+    return [0, 0];
+};
+
+function getCharChangeCount(targetChar) {
+    if ("ZYXWVUTSRQPO".includes(targetChar)) {
+        return "ZYXWVUTSRQPO".indexOf(targetChar) + 1;
+    }
+    if ("ABCDEFGHIJKLMN".includes(targetChar)) {
+        return "ABCDEFGHIJKLMN".indexOf(targetChar);
+    }
+}
+
+function solution(name) {
+    var answer = 0;
+    let nowIdx = 0;
+    let count = 0;
+    while (true) {
+        let [left, leftMoveIdx] = leftTargetLength(name, nowIdx);
+        let [right, rightMoveIdx] = rightTargetLength(name, nowIdx);
+        // console.log(`left ${left} leftMoveIdx ${leftMoveIdx}`);
+        // console.log(`right ${right} rightMoveIdx ${rightMoveIdx}`);
+
+        if (left === -1 || right === -1) break;
+        if (left < right) {
+            answer += left + getCharChangeCount(name[leftMoveIdx]);
+            nowIdx = leftMoveIdx;
+        } else {
+            answer += right + getCharChangeCount(name[rightMoveIdx]);
+            nowIdx = rightMoveIdx;
+        }
+        name = name.slice(0, nowIdx) + "A" + name.slice(nowIdx + 1)
+        // console.log(`${nowIdx} ${name} answer ${answer}`);
+        // count++;
+        // if (count === 4) break;
+    }
+    return answer;
+}
