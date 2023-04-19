641e14eb530e6ac0e6e4e758dfe8e69e3060c39e
jhon3242 <rldotnr2@gmail.com>
2023-04-19 12:39:48 +0900

    feat(programers) : dfs 문제풀이 추가

--- CHOI_algorithm/programers/[Lv2]네트워크 .js
+++ CHOI_algorithm/programers/[Lv2]네트워크 .js
@@ -1,33 +1,27 @@
 /**
- * find-union 풀이
+ * DFS 풀이
  */
+function dfs(node, visited, computers) {
+    visited[node] = true;
 
-function findParent(parents, n) {
-    if (n !== parents[n]) parents[n] = findParent(parents, parents[n]);
-    return parents[n];
-}
+    for (let i = 0; i < computers.length ; i++) {
+        if (computers[node][i] && !visited[i]) {
+            dfs(i, visited, computers);
+        }
 
-function unionParents(parents, a, b) {
-    a = findParent(parents, a);
-    b = findParent(parents, b);
-    if (a < b) {
-        parents[b] = a;
-    } else {
-        parents[a] = b;
     }
 }
 
 function solution(n, computers) {
     var answer = 0;
-    let parents = new Array(n + 1).fill(0).map((v, i) => i);
+    let visited = [];
 
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (i !== j && computers[i][j]) {
-                unionParents(parents, i + 1, j + 1);
-            }
+    for (let i = 0; i < n ; i ++) {
+        if (!visited[i]) {
+            answer++;
+            dfs(i, visited, computers);
         }
     }
-    parents = parents.map(v => findParent(parents, v))
-    return [...new Set(parents)].length - 1;
+
+    return answer;
 }
